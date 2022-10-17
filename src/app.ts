4f2f7f0c677ab473282653e20ca33dadd68e3c54
jun <ggprgrkjh@naver.com>
2022-10-18 04:20:33 +0900

    feat : separate route modularization

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { Cat, CatType } from "./app.model";
+import catsRouter from "./cats/cats.route";
 
 const app: express.Express = express();
 
@@ -15,53 +15,10 @@ app.use((req, res, next) => {
 //* json middleware
 app.use(express.json());
 
+app.use(catsRouter);
+
 //* READ 고양이 전체 데이터 다 조회
 //* READ 특정 고양이 데이터 조회
-
-app.get("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* CREATE 새로운 고양이 추가 api
-app.post("/cats", (req, res) => {
-  try {
-    const data = req.body;
-    console.log(data);
-    Cat.push(data); // create
-    res.status(200).send({
-      success: true,
-      data,
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-// * 404 middleware
-app.use((req, res, next) => {
-  console.log("this is logging middleware");
-  res.send({ error: "404 not found error" });
-});
 
 app.listen(8000, () => {
   console.log("server is on...");
---
+++ src/cats/cats.route.ts
@@ -0,0 +1,68 @@
+import { Cat, CatType } from "./cats.model";
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+router.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가 api
+router.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data,
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+// * 404 middleware
+router.use((req, res, next) => {
+  console.log("this is logging middleware");
+  res.send({ error: "404 not found error" });
+});
+
+export default router;
