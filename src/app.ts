a1646bcd5ad20cd11a301b3608ccfe3a89f6b2e0
jun <ggprgrkjh@naver.com>
2022-10-18 04:09:18 +0900

    feat : cat CRUD

--- src/app.ts
+++ src/app.ts
@@ -5,29 +5,59 @@ const app: express.Express = express();
 
 const data = [1, 2, 3, 4];
 
+//* logging middleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[0]);
   console.log("this is logging middleware");
   next();
 });
 
-app.use("/cats/som", (req, res, next) => {
-  console.log("this is som middleware");
-  next();
+//* json middleware
+app.use(express.json());
+
+//* READ 고양이 전체 데이터 다 조회
+//* READ 특정 고양이 데이터 조회
+
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
+//* CREATE 새로운 고양이 추가 api
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data,
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/blue", (req, res) => {
-  res.send({ blue: Cat[0] });
-});
-
-app.get("/cats/som", (req, res) => {
-  res.send({ som: Cat[1] });
-});
-
+// * 404 middleware
 app.use((req, res, next) => {
   console.log("this is logging middleware");
   res.send({ error: "404 not found error" });
