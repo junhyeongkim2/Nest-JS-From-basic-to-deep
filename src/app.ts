2d01fe0b8b24b35a09d1cd6ff208ab2d69ab31c5
jun <ggprgrkjh@naver.com>
2022-10-18 03:42:35 +0900

    feat: middleware

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,32 @@ const app: express.Express = express();
 
 const data = [1, 2, 3, 4];
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[0]);
+  console.log("this is logging middleware");
+  next();
+});
+
+app.use("/cats/som", (req, res, next) => {
+  console.log("this is som middleware");
+  next();
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log("this is logging middleware");
+  res.send({ error: "404 not found error" });
 });
 
 app.listen(8000, () => {
