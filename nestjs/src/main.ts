4602bd42ce0d198e0778fceed4d4848194eb2506
jun <ggprgrkjh@naver.com>
2023-03-24 01:05:35 +0900

    feat: swagger api security

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -3,11 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // * 등록 !
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('C.I.C')
